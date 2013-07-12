5ff0e9ce94fb699c879175c6f7a2b6200d26fccf
sprhawk <hongbo@yang.me>
2013-07-12 11:28:54 +0800

    initially added

---
+++ mqtt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mqtt
+//
+//  Created by YANG HONGBO on 2013-7-10.
+//  Copyright (c) 2013年 YANG HONGBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
